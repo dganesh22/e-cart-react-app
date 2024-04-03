d8c0690f85cb84cf4c02d8fa1946d4fd325de8c1
dganesh22 <dganesh2022@gmail.com>
2024-04-03 12:12:15 +0530

    completed order

--- controller/orderCtrl.js
+++ controller/orderCtrl.js
@@ -4,7 +4,23 @@ const { StatusCodes } = require('http-status-codes')
 // create
 const createOrder = async (req,res) => {
     try {
-        return res.json({ msg: "create order"})
+        let { cartId, userId, paymentId, paymentMode, paymentStatus, orderStatus, deliveryStatus } = req.body
+
+        let extOrder = await Order.findOne({ cartId })
+            if(extOrder)
+                return res.status(StatusCodes.CONFLICT).json({ status: false, msg: `Order is already confirmed for cart items`})
+
+        let newOrder = await Order.create({
+            cartId,
+            userId,
+            paymentId,
+            paymentMode,
+            paymentStatus,
+            orderStatus,
+            deliveryStatus
+        })
+
+        return res.status(StatusCodes.CREATED).json({ status:true, msg: "Order Placed successfully", order: newOrder })
     } catch (err) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status: false, msg: err.message })
     }
@@ -13,7 +29,14 @@ const createOrder = async (req,res) => {
 // all
 const allOrder = async (req,res) => {
     try {
-        return res.json({ msg: "all order"})
+            // read all orders
+        let orderData = await Order.find({})
+        // reading current user id => middleware auth
+        let id = req.userId
+            // filtering orders w.r.to current user id
+        let data = await orderData.filter(item => item.userId == id)
+
+        return res.status(StatusCodes.OK).json({ status: true, length: data.length, order: data })
     } catch (err) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status: false, msg: err.message })
     }
@@ -21,7 +44,19 @@ const allOrder = async (req,res) => {
 // single
 const singleOrder = async (req,res) => {
     try {
-        return res.json({ msg: "single order"})
+        let id = req.params.id 
+
+        let extOrder = await Order.findById(id)
+
+            if(!extOrder)
+                return res.status(StatusCodes.NOT_FOUND).json({ status: false, msg: `Requested order id not found`})
+
+        // other than current user can't authorized access order details
+            if(extOrder.userId != req.userId)
+                return res.status(StatusCodes.UNAUTHORIZED).json({ status: false, msg: `Unauthorized to see order details`})
+
+            
+        return res.status(StatusCodes.OK).json({ status: true, order: extOrder })
     } catch (err) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status: false, msg: err.message })
     }
@@ -30,7 +65,16 @@ const singleOrder = async (req,res) => {
 // update
 const updateOrder = async (req,res) => {
     try {
-        return res.json({ msg: "update order"})
+        let id = req.params.id 
+
+        let extOrder = await Order.findById(id)
+            if(!extOrder)
+                return res.status(StatusCodes.NOT_FOUND).json({ status: false, msg: `Requested order id not found`})
+
+        // update
+        await Order.findByIdAndUpdate({_id: id }, req.body)
+
+        return res.status(StatusCodes.ACCEPTED).json({ status: true, msg: "order updated successfully"})
     } catch (err) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status: false, msg: err.message })
     }
@@ -38,7 +82,16 @@ const updateOrder = async (req,res) => {
 // delete
 const deleteOrder = async (req,res) => {
     try {
-        return res.json({ msg: "delete order"})
+        let id = req.params.id 
+
+        let extOrder = await Order.findById(id)
+            if(!extOrder)
+                return res.status(StatusCodes.NOT_FOUND).json({ status: false, msg: `Requested order id not found`})
+
+        // update
+        await Order.findByIdAndDelete({_id: id })
+
+        return res.status(StatusCodes.ACCEPTED).json({ status: true, msg: "order deleted successfully"})
     } catch (err) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status: false, msg: err.message })
     }
--- model/order.js
+++ model/order.js
@@ -1,29 +1,43 @@
 const mongoose = require('mongoose')
 const Cart = require('./cart')
+const User = require('./user')
 
 const OrderSchema = new mongoose.Schema({
     cartId: {
         type: mongoose.Schema.ObjectId,
         ref: Cart
     },
+    userId: {
+        type: mongoose.Schema.ObjectId,
+        ref: User
+    },
     paymentId: {
         type: String,
         trim: true
     },
+    paymentMode: {
+        type: String,
+        trim: true,
+        enum: ["cod", "upi", "online", "wallets", "card"],
+        default: "cod"
+    },
     paymentStatus: {
         type: String,
         trim:true,
-        enum: ["pending", "success","failed"]
+        enum: ["pending", "success","failed"],
+        default: "pending"
     },
     orderStatus: {
         type: String,
         trim: true,
-        enum: ["pending", "confirmed","canceled"]
+        enum: ["pending", "confirmed","canceled"],
+        default: "pending"
     },
     deliveryStatus: {
         type: String,
         trim: true,
-        enum: ["processing", "delivered", "returned"]
+        enum: ["pending","processing", "delivered", "returned"],
+        default: "pending"
     }
 },{
     collection: "order",
